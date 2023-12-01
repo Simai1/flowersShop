dc8a3119bf0bdb86af5a651d2f387b281f05f32e
DaiTangoMne <mksim3uchnev@gmail.com>
2023-12-01 19:47:28 +0300

    add register and login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
+import authController from "../controllers/auth.js";
+import {asyncRoute} from "../utils/errors.js";
+
 const router = Router();
 
 router.get('/',  (req, res) => {
     res.send('test');
 })
+
+router.post('/login', asyncRoute(authController.login));
+
+router.post('/register', asyncRoute(authController.register));
 
 export default router;
